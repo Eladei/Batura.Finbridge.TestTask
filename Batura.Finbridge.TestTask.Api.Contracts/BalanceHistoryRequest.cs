748bbbddfc9b1b2765402d6f1564807d8ffe1de2
Eladei <batu-vladimir@yandex.ru>
2026-06-04 21:59:06 +0300

    Add api-contracts

---
+++ Batura.Finbridge.TestTask.Api.Contracts/BalanceHistoryRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Batura.Finbridge.TestTask.Api.Contracts;
+
+/// <summary>
+/// Запрос на получение истории изменения баланса пользователей
+/// </summary>
+public sealed record BalanceHistoryRequest
+{
+    /// <summary>
+    /// Номер страницы
+    /// </summary>
+    public int Page { get; init; }
+
+    /// <summary>
+    /// Количество записей на странице
+    /// </summary>
+    public int HistoryItemsPerPage { get; init; }
+}
---
+++ Batura.Finbridge.TestTask.Api.Contracts/UsersResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace Batura.Finbridge.TestTask.Api.Contracts;
+
+/// <summary>
+/// Ответ на запрос получения зарегистрированных пользователей
+/// </summary>
+public sealed record UsersResponse
+{
+    /// <summary>
+    /// Зарегистрированные пользователи
+    /// </summary>
+    public User[] Users { get; init; } = [];
+
+    /// <summary>
+    /// Общее количество страниц
+    /// </summary>
+    public int TotalPages { get; init; }
+}
