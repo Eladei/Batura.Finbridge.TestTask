3066cc45c2aace078bde5ca5910ff22caeb2406b
Eladei <batu-vladimir@yandex.ru>
2026-06-05 03:14:52 +0300

    Add QueryExecutor

---
+++ Batura.Finbridge.TestTask.Application/Queries/EfQueryBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Batura.Finbridge.TestTask.Application.Queries;
+
+/// <summary>
+/// Базовый запрос для работы с Entity Framework
+/// </summary>
+/// <typeparam name="T">Тип контекста базы данных</typeparam>
+/// <typeparam name="R">Тип результата</typeparam>
+public abstract class EfQueryBase<T, R> : IEfQuery<T, R> where T : DbContext
+{
+    /// <inheritdoc />
+    public abstract Task<R> ExecuteAsync(T context, CancellationToken cancellationToken = default);
+}
---
+++ Batura.Finbridge.TestTask.Application/Queries/IEfQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace Batura.Finbridge.TestTask.Application.Queries;
+
+/// <summary>
+/// Запрос для работы с Entity Framework
+/// </summary>
+/// <typeparam name="T">Тип контекста базы данных</typeparam>
+/// <typeparam name="R">Тип результата</typeparam>
+public interface IEfQuery<T, R>
+{
+    /// <summary>
+    /// Выполняет запрос
+    /// </summary>
+    /// <param name="context">Контекст базы данных</param>
+    /// <param name="cancellationToken">Токен отмены</param>
+    /// <returns>Результат операции</returns>
+    Task<R> ExecuteAsync(T context, CancellationToken cancellationToken);
+}
